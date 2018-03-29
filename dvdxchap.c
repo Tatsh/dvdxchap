87388288ccf9a6a5a640d436b82ae648ce3acbe8
Andrew Udvare <audvare@gmail.com>
2018-03-28 20:45:52 -0400

    Lint

--- dvdxchap.c
+++ dvdxchap.c
@@ -1,14 +1,15 @@
-/*
-  dvdxchap -- utility for extracting chapter start times from
-  IFO files
-
-  Written by Moritz Bunkus <moritz@bunkus.org>
-
-  Distributed under the GPL
-  see the file COPYING for details
-  or visit http://www.gnu.org/copyleft/gpl.html
-*/
+/**
+ * dvdxchap -- utility for extracting chapter start times from IFO files
+ *
+ * Maintained by Andrew Udvare <audvare@gmail.com>
+ * Written by Moritz Bunkus <moritz@bunkus.org>
+ *
+ * Distributed under the GPL.
+ * See the file COPYING.md for details or visit
+ * http://www.gnu.org/copyleft/gpl.html
+ */
 #include <errno.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,14 +24,16 @@ void usage() {
             "dvdxchap v" VERSION "\n"
             "Usage: dvdxchap [options] DVD-SOURCE\n\n"
             " options:\n"
-            "   -t, --title num            Use title 'num'. Default is 1.\n"
-            "   -c, --chapter start[-end]  Chapter to start at (to end at). Default 1.\n"
-            "   -v, --verbose              Increase verbosity\n"
-            "   -V, --version              Show version information\n"
-            "   -h, --help                 Show this help\n");
+            "   -t, --title num           Use title 'num'. Default is 1.\n"
+            "   -c, --chapter start[-end] Chapter to start at (to end at). "
+            "Default 1.\n"
+            "   -v, --verbose             Increase verbosity\n"
+            "   -V, --version             Show version information\n"
+            "   -h, --help                Show this help\n");
 }
 
-void display_chapters(char *source, int title, int start, int end, int verbose) {
+void display_chapters(
+    char *source, long int title, int start, int end, int verbose) {
     dvd_reader_t *dvd;
     ifo_handle_t *vmg;
     ifo_handle_t *vts;
@@ -39,12 +42,12 @@ void display_chapters(char *source, int title, int start, int end, int verbose) 
     vts_ptt_srpt_t *vts_ptt_srpt;
     pgc_t *cur_pgc;
     dvd_time_t *dt;
-    double fps;
-    long hour, minute, second, ms, overall_time, start_time, cur_time;
+    double fps, ms, cur_time, overall_time, start_time, hour, minute, second;
 
     dvd = DVDOpen(source);
     if (dvd == NULL) {
-        fprintf(stderr, "(%s) Could not open source '%s'.\n", __FILE__, source);
+        fprintf(
+            stderr, "(%s) Could not open source '%s'.\n", __FILE__, source);
         exit(1);
     }
 
@@ -56,17 +59,29 @@ void display_chapters(char *source, int title, int start, int end, int verbose) 
     }
 
     tt_srpt = vmg->tt_srpt;
-    if (verbose)
-        fprintf(stderr, "(%s) This DVD contains %d titles.\n", __FILE__, tt_srpt->nr_of_srpts);
+    if (verbose) {
+        fprintf(stderr,
+                "(%s) This DVD contains %d titles.\n",
+                __FILE__,
+                tt_srpt->nr_of_srpts);
+    }
     if (title > tt_srpt->nr_of_srpts) {
-        fprintf(stderr, "(%s) The DVD only contains %d titles.\n", __FILE__, tt_srpt->nr_of_srpts);
+        fprintf(stderr,
+                "(%s) The DVD only contains %d titles.\n",
+                __FILE__,
+                tt_srpt->nr_of_srpts);
         ifoClose(vmg);
         DVDClose(dvd);
         exit(1);
     }
     title--;
-    if (verbose)
-        fprintf(stderr, "(%s) Title %d contains %d chapters.\n", __FILE__, title + 1, tt_srpt->title[title].nr_of_ptts);
+    if (verbose) {
+        fprintf(stderr,
+                "(%s) Title %ld contains %d chapters.\n",
+                __FILE__,
+                title + 1,
+                tt_srpt->title[title].nr_of_ptts);
+    }
 
     vts = ifoOpen(dvd, tt_srpt->title[title].title_set_nr);
     if (vts == NULL) {
@@ -103,38 +118,49 @@ void display_chapters(char *source, int title, int start, int end, int verbose) 
             hour = ((dt->hour & 0xf0) >> 4) * 10 + (dt->hour & 0x0f);
             minute = ((dt->minute & 0xf0) >> 4) * 10 + (dt->minute & 0x0f);
             second = ((dt->second & 0xf0) >> 4) * 10 + (dt->second & 0x0f);
-            if (((dt->frame_u & 0xc0) >> 6) == 1)
-                fps = 25.00;
-            else
+            if (((dt->frame_u & 0xc0) >> 6) == 1) {
+                fps = 25.0;
+            } else {
                 fps = 29.97;
+            }
             dt->frame_u &= 0x3f;
-            dt->frame_u = ((dt->frame_u & 0xf0) >> 4) * 10 + (dt->frame_u & 0x0f);
-            ms = (double)dt->frame_u * 1000.0 / fps;
-            cur_time += (hour * 60 * 60 * 1000 + minute * 60 * 1000 + second * 1000 + ms);
+            dt->frame_u = (uint8_t)((((dt->frame_u & 0xf0) >> 4) * 10) +
+                                    (dt->frame_u & 0x0f));
+            ms = dt->frame_u * 1000.0 / fps;
+            cur_time += hour * 60 * 60 * 1000 + minute * 60 * 1000 +
+                        second * 1000 + ms;
         }
-        if (start == i)
+        if (start == i) {
             start_time = overall_time;
+        }
         if (i >= start && (i < end || end <= 0)) {
+            fprintf(
+                stdout,
+                "CHAPTER%02d=%02.0f:%02.0f:%02.0f.%03.0f\n",
+                i + 1 - start,
+                (overall_time - start_time) / 60 / 60 / 1000,
+                fmod((overall_time - start_time) / 60 / 1000, 60),
+                fmod((overall_time - start_time) / 1000, 60),
+                floor(1000 * fmod(overall_time - start_time, 1000) / 1000));
             fprintf(stdout,
-                    "CHAPTER%02d=%02ld:%02ld:%02ld.%03ld\n",
+                    "CHAPTER%02dNAME=Chapter %02d\n",
                     i + 1 - start,
-                    (overall_time - start_time) / 60 / 60 / 1000,
-                    ((overall_time - start_time) / 60 / 1000) % 60,
-                    ((overall_time - start_time) / 1000) % 60,
-                    (overall_time - start_time) % 1000);
-            fprintf(stdout, "CHAPTER%02dNAME=Chapter %02d\n", i + 1 - start, i + 1 - start);
+                    i + 1 - start);
         }
         overall_time += cur_time;
     }
     if (end <= 0 || i == end) {
         fprintf(stdout,
-                "CHAPTER%02d=%02ld:%02ld:%02ld.%03ld\n",
+                "CHAPTER%02d=%02.0f:%02.0f:%02.0f.%03.0f\n",
                 i + 1 - start,
                 (overall_time - start_time) / 60 / 60 / 1000,
-                ((overall_time - start_time) / 60 / 1000) % 60,
-                ((overall_time - start_time) / 1000) % 60,
-                (overall_time - start_time) % 1000);
-        fprintf(stdout, "CHAPTER%02dNAME=Chapter %02d\n", i + 1 - start, i + 1 - start);
+                fmod((overall_time - start_time) / 60 / 1000, 60),
+                fmod((overall_time - start_time) / 1000, 60),
+                floor(1000 * fmod(overall_time - start_time, 1000)) / 1000);
+        fprintf(stdout,
+                "CHAPTER%02dNAME=Chapter %02d\n",
+                i + 1 - start,
+                i + 1 - start);
     }
 
     ifoClose(vts);
@@ -143,7 +169,7 @@ void display_chapters(char *source, int title, int start, int end, int verbose) 
 }
 
 int main(int argc, char *argv[]) {
-    int title = 1;
+    long int title = 1;
     int start = 0, end = 0;
     int i;
     char *source = NULL;
@@ -164,22 +190,32 @@ int main(int argc, char *argv[]) {
             exit(0);
         } else if (!strcmp(argv[i], "-t") || !strcmp(argv[i], "--title")) {
             if ((i + 1) >= argc) {
-                fprintf(stderr, "(%s) Error: -t lacks a title number.\n", __FILE__);
+                fprintf(stderr,
+                        "(%s) Error: -t lacks a title number.\n",
+                        __FILE__);
                 exit(1);
             }
             title = strtol(argv[i + 1], NULL, 10);
             if ((errno == ERANGE) || (errno == EINVAL) || (title < 1)) {
-                fprintf(stderr, "(%s) Error: '%s' is not a valid title number.\n", __FILE__, argv[i + 1]);
+                fprintf(stderr,
+                        "(%s) Error: '%s' is not a valid title number.\n",
+                        __FILE__,
+                        argv[i + 1]);
                 exit(1);
             }
             i++;
         } else if (!strcmp(argv[i], "-c") || !strcmp(argv[i], "--chapter")) {
             if ((i + 1) >= argc) {
-                fprintf(stderr, "(%s) Error: -c lacks a chapter number.\n", __FILE__);
+                fprintf(stderr,
+                        "(%s) Error: -c lacks a chapter number.\n",
+                        __FILE__);
                 exit(1);
             }
             if (sscanf(argv[i + 1], "%d-%d", &start, &end) < 1) {
-                fprintf(stderr, "(%s) Error: '%s' is not a valid chapter range.\n", __FILE__, argv[i + 1]);
+                fprintf(stderr,
+                        "(%s) Error: '%s' is not a valid chapter range.\n",
+                        __FILE__,
+                        argv[i + 1]);
                 exit(1);
             }
             if (start < 0) {
@@ -192,12 +228,15 @@ int main(int argc, char *argv[]) {
                 start = end;
                 end = tmp;
             }
-            if (start > 0)
+            if (start > 0) {
                 start--;
+            }
             i++;
         } else {
             if (source != NULL) {
-                fprintf(stderr, "(%s) Error: more than one source given.\n", __FILE__);
+                fprintf(stderr,
+                        "(%s) Error: more than one source given.\n",
+                        __FILE__);
                 exit(1);
             }
             source = argv[i];
